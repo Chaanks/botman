da3ebdb7b2acab0399862945a750b3a5736deb2a
Chaanks <jarod8405@gmail.com>
2025-10-10 22:11:05 +0200

    update botman

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-"""Main entry point for Bot Manager application."""
-
 from botman.web.server import serve
 
 if __name__ == "__main__":
-    serve(host="100.115.85.125", port=5173)
+    serve(appname="botman.web.server", host="100.115.85.125", port=5173, reload=False)
